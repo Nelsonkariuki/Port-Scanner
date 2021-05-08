51f0a0caf053636a378d15606abfa861efae3767
Nelsonkariuki <56246437+Nelsonkariuki@users.noreply.github.com>
2021-05-08 16:02:55 +0300

    Add files via upload
    python script

---
+++ main.py
@@ -0,0 +1,28 @@
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+   
+ascii_banner = pyfiglet.figlet_format("NELSON")
+print(ascii_banner)
+
+
+
+
+from socket import *
+import time
+startTime = time.time()
+
+if __name__ == '__main__':
+   target = input('Enter the host to be scanned: ')
+   t_IP = gethostbyname(target)
+   print ('Starting scan on host: ', t_IP)
+   
+   for i in range(50, 500):
+      s = socket(AF_INET, SOCK_STREAM)
+      
+      conn = s.connect_ex((t_IP, i))
+      if(conn == 0) :
+         print ('Port %d: OPEN' % (i,))
+      s.close()
+print('Time taken:', time.time() - startTime)
